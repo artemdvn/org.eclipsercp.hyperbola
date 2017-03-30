52557dffaa41de9da5d0a5cf0c632a6b2dc98b3d
H179738 <H179738@ADVORNICHENKO.rdx.lgc.com>
2017-03-30 18:07:43 +0300

    drag and drop (not finished)

--- src/org/eclipsercp/hyperbola/view/MyView.java
+++ src/org/eclipsercp/hyperbola/view/MyView.java
@@ -5,9 +5,20 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DragSource;
+import org.eclipse.swt.dnd.DragSourceEvent;
+import org.eclipse.swt.dnd.DragSourceListener;
+import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.DropTargetListener;
+import org.eclipse.swt.dnd.FileTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
@@ -16,6 +27,7 @@ import org.eclipse.ui.part.ViewPart;
 import org.eclipsercp.hyperbola.controller.NodeController;
 import org.eclipsercp.hyperbola.model.ElementNode;
 import org.eclipsercp.hyperbola.model.GroupNode;
+import org.eclipsercp.hyperbola.model.INode;
 import org.eclipsercp.hyperbola.service.PropertyService;
 
 public class MyView extends ViewPart {
@@ -80,6 +92,8 @@ public class MyView extends ViewPart {
 		// make the viewer selection available
 		getSite().setSelectionProvider(tv);
 
+		implementDragAndDrop();
+
 		tv.addDoubleClickListener(doubleClickListenerForTreeViewer);
 		tv.expandAll();
 
@@ -96,5 +110,128 @@ public class MyView extends ViewPart {
 		tv.removeDoubleClickListener(doubleClickListenerForTreeViewer);
 		super.dispose();
 	}
+
+	private void implementDragAndDrop() {
+		// Allow data to be copied or moved from the drag source
+		int operations = DND.DROP_MOVE | DND.DROP_COPY | DND.DROP_DEFAULT;
+		DragSource source = new DragSource(tv.getTree(), operations);
+
+		// Provide data in Text format
+		Transfer[] types = new Transfer[] { TextTransfer.getInstance() };
+		source.setTransfer(types);
+
+		source.addDragListener(new DragSourceListener() {
+			public void dragStart(DragSourceEvent event) {
+				// Only start the drag if there is tree node selected.
+				//if (tv.getTree().getSelection().length == 0) {
+				//	event.doit = false;
+				//}
+			}
+
+			public void dragSetData(DragSourceEvent event) {
+				// Provide the data of the requested type.
+				if (TextTransfer.getInstance().isSupportedType(event.dataType)) {
+					IStructuredSelection selection = tv.getStructuredSelection();
+					INode firstElement = (INode) selection.getFirstElement();
+					event.data = String.valueOf(firstElement.getId());
+				}
+			}
+
+			public void dragFinished(DragSourceEvent event) {
+				// If a move operation has been performed, remove the data
+				// from the source
+				//if (event.detail == DND.DROP_MOVE) {}
+				// tv.getTree().setText("");
+
+			}
+		});
+		
+		
+		
+		// Allow data to be copied or moved to the drop target
+		DropTarget target = new DropTarget(tv.getTree(), operations);
+
+		// Receive data in Text or File format
+		final TextTransfer textTransfer = TextTransfer.getInstance();
+		final FileTransfer fileTransfer = FileTransfer.getInstance();
+		types = new Transfer[] { fileTransfer, textTransfer };
+		target.setTransfer(types);
+
+		target.addDropListener(new DropTargetListener() {
+			public void dragEnter(DropTargetEvent event) {
+				//event.detail = DND.DROP_COPY;
+//				if (event.detail == DND.DROP_DEFAULT) {
+//					if ((event.operations & DND.DROP_COPY) != 0) {
+//						event.detail = DND.DROP_COPY;
+//					} else {
+//						event.detail = DND.DROP_NONE;
+//					}
+//				}
+//				// will accept text but prefer to have files dropped
+//				for (int i = 0; i < event.dataTypes.length; i++) {
+//					if (fileTransfer.isSupportedType(event.dataTypes[i])) {
+//						event.currentDataType = event.dataTypes[i];
+//						// files should only be copied
+//						if (event.detail != DND.DROP_COPY) {
+//							event.detail = DND.DROP_NONE;
+//						}
+//						break;
+//					}
+//				}
+			}
+
+			public void dragOver(DropTargetEvent event) {
+//				event.feedback = DND.FEEDBACK_SELECT | DND.FEEDBACK_SCROLL;
+//				if (textTransfer.isSupportedType(event.currentDataType)) {
+//					// NOTE: on unsupported platforms this will return null
+//					Object o = textTransfer.nativeToJava(event.currentDataType);
+//					String t = (String) o;
+//					if (t != null)
+//						System.out.println(t);
+//				}
+			}
+
+			public void dragOperationChanged(DropTargetEvent event) {
+//				if (event.detail == DND.DROP_DEFAULT) {
+//					if ((event.operations & DND.DROP_COPY) != 0) {
+//						event.detail = DND.DROP_COPY;
+//					} else {
+//						event.detail = DND.DROP_NONE;
+//					}
+//				}
+//				// allow text to be moved but files should only be copied
+//				if (fileTransfer.isSupportedType(event.currentDataType)) {
+//					if (event.detail != DND.DROP_COPY) {
+//						event.detail = DND.DROP_NONE;
+//					}
+//				}
+			}
+
+			public void dragLeave(DropTargetEvent event) {
+			}
+
+			public void dropAccept(DropTargetEvent event) {
+			}
+
+			public void drop(DropTargetEvent event) {
+				if (textTransfer.isSupportedType(event.currentDataType)) {
+					int nodeId = Integer.parseInt((String) event.data);
+					INode draggedNode = NodeController.getInstance().getNodeById(nodeId);
+					System.out.println(draggedNode.getTitle());
+					// TableItem item = new TableItem(dropTable, SWT.NONE);
+					// item.setText(text);
+				}
+				if (fileTransfer.isSupportedType(event.currentDataType)) {
+					String[] files = (String[]) event.data;
+					// for (int i = 0; i < files.length; i++) {
+					// TableItem item = new TableItem(dropTable, SWT.NONE);
+					// item.setText(files[i]);
+					// }
+				}
+			}
+		});
+		
+	}
+	
 
 }
